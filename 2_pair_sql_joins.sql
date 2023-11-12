858262d44e71a3ed83c22c5aa0c6f3690babdbd8
Marga Obrador <marmodovar@gmail.com>
2023-11-12 17:08:24 +0100

    pair joins finished

--- 2_pair_sql_joins.sql
+++ 2_pair_sql_joins.sql
@@ -145,14 +145,53 @@ orders.order_date;
 
 
 -- 9. Ejercicio 9: Empleadas y sus supervisoras: 
+-- Concretamente nos piden: la ubicación, nombre, y apellido tanto de las empleadas como de las jefas. Investiga el resultado, ¿sabes decir quién es el director?
 
  -- falta el 9. Esta se la parte de César 
-SELECT * FROM employees; 
+
 SELECT * 
 FROM employees AS e1, employees AS e2 
 WHERE e1.employee_number = e2.reports_to; 
 
+SELECT 
+    e1.employee_number AS Numero_Empleado,
+    e1.first_name AS Nombre_Empleado,
+    e1.last_name AS Apellido_Empleado,
+    o1.city AS Ciudad_empleado,
+    e2.first_name AS Nombre_supervisor,
+    e2.last_name AS Apellido_supervisor,
+    o2.city AS Ciudad_supervisor
+    
+FROM 
+    employees AS e1
+JOIN
+    employees AS e2 ON e1.reports_to = e2.employee_number
+JOIN
+    offices AS o1 ON e1.office_code = o1.office_code
+JOIN
+    offices AS o2 ON e2.office_code = o2.office_code;
 
 
+-- ejercicio 10: Selecciona todos los pedidos, tengan empresa asociada o no, y todas las empresas tengan pedidos asociados o no. 
+-- Muestra el ID del pedido, el nombre de la empresa y la fecha del pedido (si existe).
+
+
+SELECT
+    orders.order_number AS ID_Pedido,
+    customers.customer_name AS Nombre_Empresa,
+    orders.order_date AS Fecha_Pedido
+FROM orders 
+LEFT JOIN customers ON orders.customer_number = customers.customer_number
+
+UNION
+
+SELECT
+    orders.order_number AS ID_Pedido,
+    customers.customer_name AS Nombre_Empresa,
+    orders.order_date AS Fecha_Pedido
+
+FROM orders
+RIGHT JOIN customers ON orders.customer_number = customers.customer_number
+WHERE orders.order_number IS NULL;
 
 
