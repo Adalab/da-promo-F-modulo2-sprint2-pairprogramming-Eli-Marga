a8f62e20be02aa4972dc87987a090b8e54f53a0e
Marga Obrador <marmodovar@gmail.com>
2023-11-20 13:10:21 +0100

    4 subqueries ej7

--- 4_pair_subconsultas.sql
+++ 4_pair_subconsultas.sql
@@ -1,58 +1,52 @@
+USE northwind;
 
-USE northwind; 
--- 1. Extraed los pedidos con el máximo "order_date" para cada empleado.
--- Nuestro jefe quiere saber la fecha de los pedidos más recientes que ha gestionado cada empleado. 
--- Para eso nos pide que lo hagamos con una query correlacionada.
+/*1.Extraed los pedidos con el máximo "order_date" para cada empleado.
+Nuestro jefe quiere saber la fecha de los pedidos más recientes que ha gestionado cada empleado. Para eso nos pide que lo hagamos con una query correlacionada.
+Los resultados de esta query serán*/
 
-SELECT
-    orders.employee_id AS "ID employee",
-    orders.order_date AS "Max order date"
-FROM orders
-WHERE orders.order_date = (
-    SELECT MAX(o.order_date)
-    FROM orders o
-    WHERE o.employee_id = orders.employee_id
-);
+SELECT MAX(ord1.order_date) AS "Date Max of order",ord1.customer_id AS "Client's ID",ord1.employee_id AS "Employee's ID",ord1.required_date AS "Required Date",ord1.order_id AS "Order ID"
+FROM orders AS ord1
+WHERE ord1.order_date =
+(SELECT MAX(ord2.order_date)
+FROM orders AS ord2
+WHERE ord1.employee_id=ord2.employee_id)
+GROUP BY ord1.customer_id,ord1.employee_id,ord1.required_date,ord1.order_id;
 
+/*2.Extraed el precio unitario máximo (unit_price) de cada producto vendido.
+Supongamos que ahora nuestro jefe quiere un informe de los productos vendidos y su precio unitario. De nuevo lo tendréis que hacer con queries correlacionadas.
+Los resultados deberán ser:*/
 
--- 2. Extraed el precio unitario máximo (unit_price) de cada producto vendido.
--- Supongamos que ahora nuestro jefe quiere un informe de los productos vendidos y su precio unitario. 
--- De nuevo lo tendréis que hacer con queries correlacionadas.
+SELECT MAX(ord_details1.unit_price) AS "Price per unit" ,ord_details1.product_id AS "Product ID"
+FROM order_details AS ord_details1
+WHERE unit_price = 
+(SELECT MAX(ord_details2.unit_price)
+FROM order_details AS ord_details2
+WHERE ord_details1.product_id= ord_details2.product_id)
+GROUP BY ord_details1.unit_price, ord_details1.product_id;
 
-SELECT products.product_id AS "ID product", products.product_name "Name", orders.max_unit_price "Max price"
-FROM products
-JOIN (SELECT product_id, MAX(unit_price) AS max_unit_price
-	  FROM order_details
-	  GROUP BY product_id
-	) orders ON products.product_id = orders.product_id;
+/*3.Extraed información de los productos "Beverages"*/
+/*En este caso nuestro jefe nos pide que le devolvamos toda la información necesaria para identificar un tipo de producto. */
+/*En concreto, tienen especial interés por los productos con categoría "Beverages". */
+/*Devuelve el ID del producto, el nombre del producto y su ID de categoría.*/
 
--- 3. Extraed información de los productos "Beverages"
--- En concreto, tienen especial interés por los productos con categoría "Beverages". 
--- Devuelve el ID del producto, el nombre del producto y su ID de categoría.
+SELECT p.product_id AS "Product ID",p.product_name AS "Name",p.category_id AS "Category"
+FROM products AS p
+WHERE p.category_id=
+(SELECT c.category_id
+FROM categories AS c
+WHERE c.category_name= "Beverages");
 
-SELECT product_id AS "Id product", product_name AS "Name", category_id AS "Category"
-FROM products
-WHERE category_id = (
-    SELECT category_id
-    FROM categories
-    WHERE category_name = 'Beverages'
-);
+/*4.Extraed la lista de países donde viven los clientes, pero no hay ningún proveedor ubicado en ese país*/
 
-
--- 4. Extraed la lista de países donde viven los clientes, pero no hay ningún proveedor ubicado en ese país
--- Suponemos que si se trata de ofrecer un mejor tiempo de entrega a los clientes, 
--- entonces podría dirigirse a estos países para buscar proveedores adicionales.
-
-SELECT DISTINCT customers.country AS "Client's Country"
+SELECT distinct country AS "Client's Country"
 FROM customers
-WHERE customers.country NOT IN (
-    SELECT DISTINCT suppliers.country
-    FROM suppliers
-);
-
--- 5. Extraer los clientes que compraron mas de 20 articulos "Grandma's Boysenberry Spread"
--- Extraed el OrderId y el nombre del cliente que pidieron más de 20 artículos del producto "Grandma's Boysenberry Spread" 
--- (ProductID 6) en un solo pedido.
+WHERE country NOT IN (SELECT DISTINCT country
+        FROM suppliers
+        WHERE country IS NOT NULL);
+        
+/*5.Extraer los clientes que compraron mas de 20 articulos "Grandma's Boysenberry Spread"*/
+/*Extraed el OrderId y el nombre del cliente que pidieron */
+/*más de 20 artículos del producto "Grandma's Boysenberry Spread" (ProductID 6) en un solo pedido.*/
 
 SELECT orders.customer_id AS "ID client", customers.company_name AS "Company Name", order_details.order_id AS "Order ID", order_details.quantity AS "Quantity"
 FROM orders 
@@ -62,17 +56,15 @@ WHERE order_details.product_id = 6
 AND order_details.quantity > 20;
 
 
--- 6. Extraed los 10 productos más caros
--- Nos siguen pidiendo más queries correlacionadas. 
--- En este caso queremos saber cuáles son los 10 productos más caros.
+/*6.Extraed los 10 productos más caros*/
+/*Nos siguen pidiendo más queries correlacionadas. En este caso queremos saber cuáles son los 10 productos más caros.*/
 
-SELECT product_id AS "ID product", product_name AS "Name", unit_price AS "Price per unit"
-FROM products p1
-WHERE 10 > (
-    SELECT COUNT(DISTINCT p2.unit_price)
-    FROM products p2
-    WHERE p2.unit_price > p1.unit_price
-);
+SELECT product_name AS "Name", unit_price AS "Price per unit"
+FROM products as pr
+WHERE product_name = (SELECT product_name
+FROM products as pr1 where pr.product_name = pr1.product_name)
+ORDER BY unit_price DESC
+LIMIT 10;
 
 -- 7. Qué producto es más popular
 -- Extraed cuál es el producto que más ha sido comprado y la cantidad que se compró.
@@ -90,3 +82,5 @@ GROUP BY
 ORDER BY 
     SUM(order_details.quantity)DESC
 LIMIT 1;
+
+
